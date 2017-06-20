58d6cdf3406a28650a975be73b174139e7b0568b
Rafael Sônego da Silva <rafael2785@gmail.com>
2017-06-20 06:16:54 -0400

    Update annotation Test

--- src/test/java/br/com/rafaelsonego/md5/test/HashTest.java
+++ src/test/java/br/com/rafaelsonego/md5/test/HashTest.java
@@ -1,5 +1,7 @@
 package br.com.rafaelsonego.md5.test;
 
+import org.junit.Test;
+
 import br.com.rafaelsonego.service.GenerateMD5Service;
 import br.com.rafaelsonego.service.impl.GenerateMD5ServiceImpl;
 import junit.framework.TestCase;
@@ -9,6 +11,7 @@ public class HashTest extends TestCase {
 	private final String pwd = "RafaelSonegodoPassword";
 	private final String hash = "E200A6BAF426C8650B91BD6766D240FE";
 	
+	@Test
 	public void testEncryptPassword(){
 		GenerateMD5Service md5 = new GenerateMD5ServiceImpl();
 		String hashGenerated = md5.encryptPassword(pwd);
@@ -16,6 +19,7 @@ public class HashTest extends TestCase {
 		
 	}
 	
+	@Test
 	public void testValidatePassword(){
 		GenerateMD5Service md5 = new GenerateMD5ServiceImpl();
 		assertTrue(md5.validatePassword(pwd, hash));
